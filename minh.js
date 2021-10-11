61386f706f5270136fc609b3fd9af61781257549
ngominh <ngominhth87@gmail.com>
2021-10-11 13:07:55 +0700

    toan tu

---
+++ minh.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeiht;
+inputWidth=prompt("Nhập chiều dài");
+inputHeight=prompt("nhập chiều rộng");
+let width=parseInt(inputWidth);
+let height=parseInt(inputHeight);
+let dientich=width*height;
+document.write("diện tích =" +dientich);
